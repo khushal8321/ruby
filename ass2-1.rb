2cf5d1194ab08ef6eb0c2e3105c1f5c3b832a70e
khushal8321 <khushal.patel@bacancy.com>
2023-02-23 14:08:43 +0530

    Assignment 2 completed.

---
+++ ass2-1.rb
@@ -0,0 +1,14 @@
+$i=0
+$j=4
+until $i>$j do
+  
+  $k=0
+  until $k>$i do
+    
+    $k+=1
+    print "#{$k}"
+  end
+   print "\n" 
+  $i+=1
+  
+end
---
+++ ass2-2.rb
@@ -0,0 +1,14 @@
+
+$i = 5
+$num = 0
+
+while $i > $num  do
+    $c=1
+    while $c<=$i do
+   
+    $c+=1
+    print("*")
+   end
+   print("\n")
+   $i-=1
+end
